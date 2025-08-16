13c4f8e69f85fa7b34c311a3f23f68c90d3d890f
Crey <spcrey@outlook.com>
2025-08-16 17:07:09 +0800

    为 ComponentActivity 添加点击事件

--- android/app/src/main/java/com/rnandroidcodeenv/MainComposeActivity.kt
+++ android/app/src/main/java/com/rnandroidcodeenv/MainComposeActivity.kt
@@ -1,26 +1,33 @@
 package com.rnandroidcodeenv
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
 import com.rnandroidcodeenv.ui.theme.AndroidTheme
 
 class MainComposeActivity : ComponentActivity() {
+
+    companion object {
+        const val TAG = "MainComposeActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             AndroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    Content(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,9 +37,13 @@ class MainComposeActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(modifier: Modifier = Modifier) {
+fun Content(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
     Text(
         text = "ComposeActivity",
         modifier = modifier
+            .clickable {
+                Toast.makeText(context, "clicked", Toast.LENGTH_SHORT).show()
+            }
     )
 }
