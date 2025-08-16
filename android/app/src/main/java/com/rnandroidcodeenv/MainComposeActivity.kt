03b94af93f769e401779b31ecb10b33ccea1c6b5
Crey <spcrey@outlook.com>
2025-08-16 16:52:03 +0800

    添加 ComponentActivity

---
+++ android/app/src/main/java/com/rnandroidcodeenv/MainComposeActivity.kt
@@ -0,0 +1,38 @@
+package com.rnandroidcodeenv
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.rnandroidcodeenv.ui.theme.AndroidTheme
+
+class MainComposeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(modifier: Modifier = Modifier) {
+    Text(
+        text = "ComposeActivity",
+        modifier = modifier
+    )
+}
